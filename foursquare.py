84fb7c59eb91ccf1ac5862d62e9544368177a351
mariamsaeedi <mariamsaeedi@live.co.uk>
2016-11-14 18:44:39 +0000

    log
    log

--- foursquare.py
+++ foursquare.py
@@ -1,9 +1,3 @@
-# ------- FOURSQUARE API KEY ------- 
-
-
-# Client ID: 0XFBHJXOKIQBB3FZSJJEGJFHS0WOJI5ZV3ZFDCBCWC2AVLH2
-# Client SECRET: 2R0RBMTKV3LTRIPR1INR4LFQ2BNPL4FP4GG4AS2F1C4N4ZIO
-
 #---------Geocode API Key ---------
 
 <<<<<<< HEAD
@@ -23,7 +17,15 @@ ll=
 # ------- Query URL ------- 
 >>>>>>> origin/master
 
+# ------- FOURSQUARE API KEY ------- 
+
+
+# Client ID: 0XFBHJXOKIQBB3FZSJJEGJFHS0WOJI5ZV3ZFDCBCWC2AVLH2
+# Client SECRET: 2R0RBMTKV3LTRIPR1INR4LFQ2BNPL4FP4GG4AS2F1C4N4ZIO
+
+
 """https://api.foursquare.com/v2/venues/search?ll=51.504557,-0.017334
+
 &query=bars=browse
 &client_id=0XFBHJXOKIQBB3FZSJJEGJFHS0WOJI5ZV3ZFDCBCWC2AVLH2
 &client_secret=2R0RBMTKV3LTRIPR1INR4LFQ2BNPL4FP4GG4AS2F1C4N4ZIO
@@ -35,8 +37,9 @@ import requests
 
 city = raw_input ('Which city would you like to explore?')
 
-req = requests.get("https://api.foursquare.com/v2/venues/trending?ll={city_name}&oauth_token=SBI2I10VMO23IPNK3PU0AANO0JEHYKNGKZYNBB3ZTF3OL3EQ&v=20161111".format(city_name=city))
+req = requests.get("https://api.foursquare.com/v2/{venues}/trending?ll=&query=bars=browse&oauth_token=SBI2I10VMO23IPNK3PU0AANO0JEHYKNGKZYNBB3ZTF3OL3EQ&v=20161111".format(city_name=city))
 
 data =req.json()
+import pdb; pdb.set_trace()
 
-print "Here are the top trending {venues} in your desired {city}".format(city=data['name'],venues=data['city'][0]['venues'])
+print "Here are the top trending{venues}in your desired{city}".format(city=data['city'],venues=data['city'][0]['venues'])
